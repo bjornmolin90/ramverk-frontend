85cae81801f5593616dcdf818eb0c35a189fbb15
Björn Molin <bjmo21@student.bth.se>
2022-08-31 15:41:54 +0100

    -

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -5,7 +5,24 @@ import 'react-quill/dist/quill.snow.css';
 function Editor() {
   const [value, setValue] = useState('');
 
-  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+  return <ReactQuill theme="snow" value={value} onChange={setValue} modules={modules} formats={formats} />;
 }
 
 export default Editor;
+
+ const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, false] }],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image'],
+      ['clean']
+    ],
+  }
+
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ]
