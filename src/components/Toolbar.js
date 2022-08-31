982f0d09d1ae061493e17279e4ba600b52fa08a5
Björn Molin <bjmo21@student.bth.se>
2022-08-31 14:30:02 +0100

    first uppload on frontend

---
+++ src/components/Toolbar.js
@@ -0,0 +1,11 @@
+import "../styles/Toolbar.css"
+
+function Toolbar () {
+    return (
+        <div className="toolbar">
+            <p>hej</p>
+        </div>
+    );
+}
+
+export default Toolbar;
